bb3958ba84944102be2123f1b4c2ff5c5a8eab43
John Abe <johndavidabe101@gmail.com>
2025-06-28 19:26:58 -0700

    Implemented MarketDataBar

---
+++ include/engine/MarketDataBar.hpp
@@ -0,0 +1,26 @@
+/*
+
+JDA Backtesting Engine
+/include/engine/MarketDataBar.hpp -
+JohnDavid Abe
+
+*/
+
+#pragma once
+
+#include <string>
+
+/**
+ * @brief Stores information on a single bar of data for a single symbol
+ */
+struct MarketDataBar {
+
+    std::string timestamp; ///< Time at which the bar started
+
+    double open = 0.0; ///< Price for the symbol at start of bar
+    double close = 0.0; ///< Price for the symbol at end of bar
+    double high = 0.0; ///< Highest price for the symbol during the bar
+    double low =  0.0; ///< Lowest price for the symbol during the bar
+
+    double volume = 0.0; ///< Number of contracts during the bar
+};
